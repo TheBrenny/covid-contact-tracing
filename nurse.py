f3c2220ed7b9204374c9e10171b1b2298698129f
bega2550 <davidgrant2906@gmail.com>
2021-06-08 21:57:18 +1000

    10pm

--- nurse.py
+++ nurse.py
@@ -1,46 +1,40 @@
 import json
 import requests
 
-#if we cant get boolean response back from the server uncomment
-def hash_string(input):
-    encoded_string = input.encode()
-    hash_fn = hashlib.sha256()
-    hash_fn.update(encoded_string)
-    return hash_fn.hexdigest()
-
-count  = 0
+count = 0
 while 1:
     count += 1
     print("Enter nurse password: ")
     password = input() #87654321
-    password_dict = {'password': '87654321'}
+    password_dict = {'password': password}
     password_json = json.dumps(password_dict)
-    res = requests.post("http://192.168.1.15:5000/nurse_logon", json=password_json)
+    # change to https when running within heroku, change ip
+    res = requests.post("http://192.168.1.15:42069/nurse_logon", json=password_json)
 
+    print(str(res))
     if "201" in str(res):
-        logon = False
-    else:
-        logon = False
-    if (logon == True):
         print("nurse logon successful")
         break
     else:
         print("access denied")
-        count += 1
-        if (count == 3):
+        if (count >= 3):
             exit()
- # if we cant get boolean response back from the server uncomment
-   #if (hash_string(password) == \
-   #         'e24df920078c3dd4e7e8d2442f00e5c9ab2a231bb3918d65cc50906e49ecaef4'):
-   #     print("nurse logon successful")
 
-   #     break
-    #else:
-     #   print("access denied")
-      #  count += 1
-       # if(count == 3):
-        #    exit()
-
-#enter ph# into server to alert users
-
-
+while 1:
+    print("\n\t1. Alert potentially infected users to nearby COVID case\n\t0. Exit")
+    selection = input()
+    if selection == '1':
+        print("Enter the phone number of the infected person in the format [+614xxxxxxxx]")
+        ph = input()
+        ph_dict = {'phone_number': ph}
+        ph_json = json.dumps(ph_dict)
+        # change to https when running within heroku, change ip
+        res = requests.post("http://192.168.1.15:5000/nurse_add_data", json=ph_dict)
+        print(ph_json)
+        print("\nAlerts sent, enter any key to return to menu")
+        input()
+    elif selection == '0':
+        print("Goodbye")
+        exit()
+    else:
+        print("Invalid user input")
