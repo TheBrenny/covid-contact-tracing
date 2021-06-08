8eaf66c50264b5a83bc38899ae4384d9ff9f72a1
TheBrenny <iam+dev@justbrenny.me>
2021-06-08 23:56:39 +1000

    clusters aren't dbs

--- server.py
+++ server.py
@@ -43,7 +43,7 @@ def covidAlert(phone_number):
 #########################################DB COMMANDS###################################################################
 def conDB():  # Creates a connection to the database **ONLY WORKS ON LOCALHOST**
     conn = pymongo.MongoClient(os.environ['DB_URL'])
-    db = conn[os.environ['DB_CLUSTER']]
+    db = conn.pfsdb
     return db
 
 
